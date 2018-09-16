cc3a5149f22e3744a25e635707bfcd1e5977b98d
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-16 13:34:17 +0700

    Add Destructure Object

---
+++ nextgenjavascript/destructure-object.js
@@ -0,0 +1,8 @@
+const myObj = {
+  name: 'Max',
+  age: 28
+}
+const {name} = myObj;
+console.log(name); // prints 'Max'
+//console.log(age);  // prints undefined
+console.log(myObj); // prints {name: 'Max', age: 28}
