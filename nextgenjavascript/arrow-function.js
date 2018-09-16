aef39896d68342e007c20e4cff6e5bb1b3853b4c
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-16 13:18:57 +0700

    Add Spread Operator for Array

--- nextgenjavascript/arrow-function.js
+++ nextgenjavascript/arrow-function.js
@@ -1,4 +1,10 @@
 const multiply = (number) => {
   return number * 2;
 }
- console.log(multiply(2));
+console.log(multiply(2));
+
+const callMe = name => {
+  console.log(name);
+}
+
+console.log(callMe("test"));
---
+++ nextgenjavascript/spread-operator-array.js
@@ -0,0 +1,4 @@
+const oldArray = [1, 2, 3];
+const newArray = [...oldArray, 4, 5]; // This now is [1, 2, 3, 4, 5];
+
+console.log(newArray);
