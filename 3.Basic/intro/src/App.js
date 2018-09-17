33476a3012d09017a4408c7555573a71f47e21e2
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-17 18:31:43 +0700

    Render Content Conditionally

--- 3.Basic/intro/src/App.js
+++ 3.Basic/intro/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person.js';
@@ -6,18 +6,33 @@ import Person from './Person/Person.js';
 class App extends Component {
   state = {
     persons: [
-      {name: "Gun", age: 26},
-      {name: "Nowo", age: 24},
-      {name: "Christa", age: 48},
-    ]
+      {
+        name: "Gun",
+        age: 26
+      }, {
+        name: "Nowo",
+        age: 24
+      }, {
+        name: "Christa",
+        age: 48
+      }
+    ],
+    showPersons: false
   }
 
   switchNameHandler = (newname) => {
     this.setState({
       persons: [
-        {name: newname, age: 26},
-        {name: "Nowo", age: 24},
-        {name: "Christa", age: 68},
+        {
+          name: newname,
+          age: 26
+        }, {
+          name: "Nowo",
+          age: 24
+        }, {
+          name: "Christa",
+          age: 68
+        }
       ]
     })
   }
@@ -25,11 +40,23 @@ class App extends Component {
   nameChangedHandler = (event) => {
     this.setState({
       persons: [
-        {name: "Gun", age: 26},
-        {name: event.target.value, age: 24},
-        {name: "Christa", age: 68},
+        {
+          name: "Gun",
+          age: 26
+        }, {
+          name: event.target.value,
+          age: 24
+        }, {
+          name: "Christa",
+          age: 68
+        }
       ]
     })
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
   }
 
   render() {
@@ -38,27 +65,34 @@ class App extends Component {
       backgroundColor: 'white',
       font: 'inherit',
       border: '1x solid pink',
-      padding: '6px',
+      padding: '6px'
     };
 
-    return (
-      <div className="App">
-        <h1 className="App-title">Welcome to React</h1>
-        <button
-          style={buttonstyle}
-          onClick={this.switchNameHandler.bind(this, 'GGF')}>
-          Switch Name
-        </button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} >Test</Person>
-        <Person
-          click={this.switchNameHandler.bind(this, 'Gun Gun Febrianza')}
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          changed={this.nameChangedHandler}
-        />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-      </div>
-    );
+    return (<div className="App">
+      <h1 className="App-title">Welcome to React</h1>
+      <button style={buttonstyle} onClick={this.togglePersonsHandler}>
+        Switch Name
+      </button>
+      {
+        this.state.showPersons === true ?
+            <div>
+              <Person
+                name={this.state.persons[0].name}
+                age={this.state.persons[0].age}>
+                Test
+              </Person>
+              <Person
+                click={this.switchNameHandler.bind(this, 'Gun Gun Febrianza')}
+                name={this.state.persons[1].name} age={this.state.persons[1].age}
+                changed={this.nameChangedHandler}/>
+              <Person
+                name={this.state.persons[2].name}
+                age={this.state.persons[2].age}/>
+            </div>
+          : null
+      }
+
+    </div>);
 
     // return (
     //   React.createElement('div',{className: 'App'}, React.createElement('h1', null, 'Test'))
