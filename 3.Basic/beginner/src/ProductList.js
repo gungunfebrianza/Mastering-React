1642a2033ebdf514b00c0bb90e62666dbc2aed5d
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-30 17:57:58 +0700

    Add Product Component

--- 3.Basic/beginner/src/ProductList.js
+++ 3.Basic/beginner/src/ProductList.js
@@ -1,38 +1,43 @@
 import React from 'react';
+import './ProductList.css';
+import Product from './Product';
+
 class ProductList extends React.Component {
   render() {
-    return (
-      <ul>
-        <li>
-          <h3>Traditional Merlot</h3>
-          <p>
-            A bottle of middle weight wine, lower in tannins (smoother), with a
-            more red-fruited flavor profile.
-          </p>
-        </li>
-        <li>
-          <h3>Classic Chianti</h3>
-          <p>
-            A medium-bodied wine characterized by a marvelous freshness with a
-            lingering, fruity finish
-          </p>
-        </li>
-        <li>
-          <h3>Chardonnay</h3>
-          <p>
-            A dry full-bodied white wine with spicy, bourbon-y notes in an
-            elegant bottle
-          </p>
-        </li>
-        <li>
-          <h3>Brunello di Montalcino</h3>
-          <p>
-            A bottle red wine with exceptionally bold fruit flavors, high
-            tannin, and high acidity
-          </p>
-        </li>
-      </ul>
-    );
+    let products = [
+      {
+        code: 'P01',
+        name: 'Traditional Merlot',
+        description:
+          'A bottle of middle weight wine, lower in tannins (smoother), with a more red-fruited flavor profile.'
+      },
+      {
+        code: 'P02',
+        name: 'Classic Chianti',
+        description:
+          'A medium-bodied wine characterized by a marvelous freshness with a lingering, fruity finish'
+      },
+      {
+        code: 'P03',
+        name: 'Chardonnay',
+        description:
+          'A dry full-bodied white wine with spicy, bourbon-y notes in an elegant bottle'
+      },
+      {
+        code: 'P04',
+        name: 'Brunello di Montalcino',
+        description:
+          'A bottle of red wine with exceptionally bold fruit flavors, high tannin, and high acidity'
+      }
+    ];
+    let productComponents = [];
+
+    for (let product of products) {
+      productComponents.push(<Product item={product} />);
+    }
+
+    return <ul>{productComponents}</ul>;
   }
 }
+
 export default ProductList;
