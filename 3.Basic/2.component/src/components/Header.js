45df1b0834e207ae9e3227943925b3b9757d8bc8
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-01 13:20:51 +0700

    Add Class-based Component

---
+++ 3.Basic/2.component/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div>Logo</div>
+        <input type="text" />
+      </header>
+    );
+  }
+}
+
+export default Header; //export component
