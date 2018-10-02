4b3c7bd654afc1d0cff13cf792b99a2e3a701d62
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-02 11:01:25 +0700

    Add State React

--- Snippets/State.js
+++ Snippets/State.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+  constructor(props) {
+    // Access Parent Component
+    super(props);
+
+    // State is an object
+    this.state = {
+      title: 'The Keywords are : ',
+      keywords: 'john'
+    };
+  }
+
   // Event Function Read Attribute
   readAttributeName(e) {
     console.log(e.target.name);
@@ -9,6 +20,11 @@ class Header extends Component {
   // Event Input Change Function Read Value
   readInputChange(e) {
     console.log(e.target.value);
+  }
+
+  // How to Change State
+  changeStateKeyword(e) {
+    this.setState({ keywords: e.target.value });
   }
 
   // Render
@@ -33,8 +49,13 @@ class Header extends Component {
         >
           Logo
         </div>
-        <input onInput={this.readInputChange} name="Test" type="text" /> //this
-        mean this class header
+        <input
+          onInput={this.changeStateKeyword.bind(this)} //this mean this class header
+          name="Test"
+          type="text"
+        />
+        <div>{this.state.title}</div>
+        <div>{this.state.keywords}</div>
       </header>
     );
   }
