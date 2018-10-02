619c3890e14ea0e66af717be3bd7dc4de0b7d593
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-03 06:35:13 +0700

    Add Initial Project

---
+++ 3.Basic/5.react-router/src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+class App extends Component {
+  render() {
+    return <div>Home</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
