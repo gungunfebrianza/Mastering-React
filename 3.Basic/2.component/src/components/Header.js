f214af40885402b7416af6baa19ea6a4967ce9d6
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-02 10:57:26 +0700

    Add State React

--- 3.Basic/2.component/src/components/Header.js
+++ 3.Basic/2.component/src/components/Header.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+  readAttributeName(e) {
+    console.log(e.target.name);
+  }
+
+  readInputChange(e) {
+    console.log(e.target.value);
+  }
+
   render() {
+    const styles = {
+      header: {
+        background: '#03a9f4'
+      },
+      logo: {
+        color: '#fff',
+        textAlign: 'center'
+      }
+    };
+
     return (
-      <header>
-        <div>Logo</div>
-        <input type="text" />
+      <header style={styles.header}>
+        <div
+          style={styles.logo}
+          onClick={() => {
+            console.log('Click');
+          }}
+        >
+          Logo
+        </div>
+        <input onInput={this.readInputChange} name="Test" type="text" />
       </header>
     );
   }
