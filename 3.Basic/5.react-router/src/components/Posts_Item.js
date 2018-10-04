cbb910bd4a85c9f09512b90afe43af17b41241d2
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-04 13:40:41 +0700

    Add React Router Example

--- 3.Basic/5.react-router/src/components/Posts_Item.js
+++ 3.Basic/5.react-router/src/components/Posts_Item.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export default class Posts_Item extends Component {
   render() {
-    //console.log(this.props);
+    console.log(this.props);
 
     return <div>POSTS ITEM {this.props.match.params.id}</div>;
   }
