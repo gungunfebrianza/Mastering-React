dc977a7102381f8ba771c8f07167b8f11121757d
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-04 18:16:02 +0700

    Add ArtistList Component

---
+++ 3.Basic/6.project-artist/src/components/Artist.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Artist extends Component {
+  render() {
+    return <div>Artist</div>;
+  }
+}
+
+export default Artist;
---
+++ 3.Basic/6.project-artist/src/components/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Banner = () => {
+  
+  return (
+    <div className="banner">
+      <div className="main_cover">
+        <div className="logo">Music DB</div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
