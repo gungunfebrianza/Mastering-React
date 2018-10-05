761f6143a9bd7d71ddc182cc9c1d0b655c7fc285
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-05 13:18:14 +0700

    Add Redux Basic

---
+++ 3.Basic/9.redux-basic/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// CONTAINERS
+import MoviesList from './containers/movies_list';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MoviesList />
+      </div>
+    );
+  }
+}
+
+export default App;
