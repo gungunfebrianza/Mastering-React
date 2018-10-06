ad41e6520a4deddd06c71de61d86f94af31c851d
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-06 18:45:07 +0700

    Create Reducer

--- 3.Basic/10.redux-intermediate/src/index.js
+++ 3.Basic/10.redux-intermediate/src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import rootReducers from './reducers';
+
+// Component
 import App from './components/App';
 import Car from './components/Car';
 
+const reduxStore = applyMiddleware()(createStore);
+
 ReactDOM.render(
-  <BrowserRouter>
-    <section>
-      <Route exact path="/" component={App} />
-      <Route path="/car/:id" component={Car} />
-    </section>
-  </BrowserRouter>,
+  <Provider store={reduxStore(rootReducers)}>
+    <BrowserRouter>
+      <section>
+        <Route exact path="/" component={App} />
+        <Route path="/car/:id" component={Car} />
+      </section>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
