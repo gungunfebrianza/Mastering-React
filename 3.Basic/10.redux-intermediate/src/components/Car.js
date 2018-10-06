f92384ccd77ddf85dff9138815692b11b456cedb
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-06 15:27:57 +0700

    Add Car & App Component

---
+++ 3.Basic/10.redux-intermediate/src/components/Car.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Car extends Component {
+  render() {
+    return <div>Car</div>;
+  }
+}
--- 3.Basic/10.redux-intermediate/src/index.js
+++ 3.Basic/10.redux-intermediate/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
+//import './App.css';
+import App from './components/App';
+import Car from './components/Car';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <BrowserRouter>
+    <section>
+      <Route exact path="/" component={App} />
+      <Route path="/car/:id" component={Car} />
+    </section>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
