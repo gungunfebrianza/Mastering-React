37fa16ac72491df16ed7a27fb647987bed79962e
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-06 15:50:15 +0700

    Seacrh Component Handling Input

--- 3.Basic/10.redux-intermediate/src/containers/Search.js
+++ 3.Basic/10.redux-intermediate/src/containers/Search.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
 
 export default class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      keyword: ''
+    };
+  }
+
+  searchCars = event => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
+  handleOnChange = event => {
+    this.setState({
+      keyword: event.target.value
+    });
+  };
+
+  componentDidMount() {
+    console.log(this.state);
+  }
+
   render() {
     return (
       <section className="main_search">
-        <form>
-          <input type="text" />
+        <form onSubmit={this.searchCars}>
+          <input
+            type="text"
+            value={this.state.keyword}
+            onChange={this.handleOnChange}
+          />
         </form>
       </section>
     );
