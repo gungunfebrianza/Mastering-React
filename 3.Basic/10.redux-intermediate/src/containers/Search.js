dd9c6e2492024ca45629445370a1b6eb74347b35
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-06 15:29:49 +0700

    Create Search Container

---
+++ 3.Basic/10.redux-intermediate/src/containers/Search.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Search extends Component {
+  render() {
+    return (
+      <section className="main_search">
+        <form>
+          <input type="text" />
+        </form>
+      </section>
+    );
+  }
+}
