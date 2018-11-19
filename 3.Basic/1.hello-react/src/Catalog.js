c66ad68a6b367ecbf872c8e8570bf26db6a30813
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-11-20 06:52:19 +0700

    Add Catalog Component

---
+++ 3.Basic/1.hello-react/src/Catalog.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Catalog extends Component {
+  render() {
+    return (
+      <div>
+        <p>Catalog</p>
+      </div>
+    );
+  }
+}
