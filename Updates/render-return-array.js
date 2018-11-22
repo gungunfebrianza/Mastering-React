66818eacd350bd26f60ba41011f17e96a3f649ef
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-11-22 17:18:47 +0700

    Render Return Array

---
+++ Updates/render-return-array.js
@@ -0,0 +1,10 @@
+// Example 1: Returning an array of elements.
+render() {
+  // Now you don't need to wrap list items in an extra element
+  return [
+    <li key="1">First item</li>,
+    <li key="2">Second item</li>,
+    <li key="3">Third item</li>,
+  ];
+}
+
