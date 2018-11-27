d941503adeb343291b550642c23605fc93d3671c
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-11-28 05:26:31 +0700

    Add Item Feature

--- 3.Basic/1.First App/10.create-react-app/src/TodoList.js
+++ 3.Basic/1.First App/10.create-react-app/src/TodoList.js
@@ -3,17 +3,42 @@ import React, { Component } from 'react';
 class TodoList extends Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      items: []
+    };
     this.addItem = this.addItem.bind(this);
   }
 
-  addItem(e) {}
+  addItem(e) {
+    var itemArray = this.state.items;
+
+    if (this._inputElement.value !== '') {
+      itemArray.unshift({
+        text: this._inputElement.value,
+        key: Date.now()
+      });
+
+      this.setState({
+        items: itemArray
+      });
+
+      this._inputElement.value = '';
+    }
+
+    console.log(itemArray);
+
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div className="todoListMain">
         <div className="header">
-          <form>
-            <input placeholder="enter task" />
+          <form onSubmit={this.addItem}>
+            <input
+              ref={a => (this._inputElement = a)}
+              placeholder="enter task"
+            />
             <button type="submit">add</button>
           </form>
         </div>
