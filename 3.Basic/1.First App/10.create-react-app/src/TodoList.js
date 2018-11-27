23c3a357f2bb606e49281f7c64f5dd352022295d
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-11-28 05:11:33 +0700

    Add Constructor

--- 3.Basic/1.First App/10.create-react-app/src/TodoList.js
+++ 3.Basic/1.First App/10.create-react-app/src/TodoList.js
@@ -1,13 +1,19 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.addItem = this.addItem.bind(this);
+  }
+
+  addItem(e) {}
   render() {
     return (
       <div className="todoListMain">
         <div className="header">
           <form>
-            <input placeholder="enter task">
-            </input>
+            <input placeholder="enter task" />
             <button type="submit">add</button>
           </form>
         </div>
