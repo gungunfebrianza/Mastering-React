0b947302160fe3fc6daf2a4ae8a53d168cadf5ac
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-11-29 05:45:08 +0700

    Render Return Fragments 2

--- Updates/react-fragments.js
+++ Updates/react-fragments.js
@@ -1,17 +1,6 @@
 
 
-// Example 2: Using React.Fragment
-render() {
-  return (
-    <React.Fragment>
-      <h1>An h1 heading</h1>
-      Some text here.
-          <h2>An h2 heading</h2>
-      More text here.
-      Even more text here.
-        </React.Fragment>
-  );
-}
+
 
 // Example 3: Importing Fragment
 import React, { Fragment } from 'react';
