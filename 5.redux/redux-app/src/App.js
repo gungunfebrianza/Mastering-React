506e4d13874e42da5ea517066c6a98a9608ed249
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-11-29 05:49:20 +0700

    Update Repo

--- 5.redux/redux-app/src/App.js
+++ 5.redux/redux-app/src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import Counter from './Counter';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+// Map Redux state to component props
+function mapStateToProps(state) {
+  return {
+    countValue: state.count
+  };
 }
 
-export default App;
+// Action
+var increaseAction = { type: 'increase' };
+var decreaseAction = { type: 'decrease' };
+
+// Map Redux actions to component props
+function mapDispatchToProps(dispatch) {
+  return {
+    increaseCount: function() {
+      return dispatch(increaseAction);
+    },
+    decreaseCount: function() {
+      return dispatch(decreaseAction);
+    }
+  };
+}
+
+// The HOC
+var connectedComponent = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
+
+export default connectedComponent;
--- 5.redux/redux-app/src/index.js
+++ 5.redux/redux-app/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import counter from './reducer';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var destination = document.querySelector('#container');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Store
+var store = createStore(counter);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  destination
+);
