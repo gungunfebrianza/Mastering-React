b92d595e6fcdfe3e5293f6902cac5b4bfd2949bd
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-12-19 15:14:24 +0700

    Add Concise Object

---
+++ 1.NextGenJavascript/concise-object.js
@@ -0,0 +1,16 @@
+const name = 'Gun Gun Febrianza';
+const age = '25';
+
+// const user = {
+//   name: name,
+//   age: age
+// };
+
+// Optimized Version
+const user = {
+  name,
+  age
+};
+
+console.log(user);
+
