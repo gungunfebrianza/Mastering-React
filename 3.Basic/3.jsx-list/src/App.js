8b418654a6518da7ae88ab7fd1654e018190618d
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-12-21 13:28:24 +0700

    Export Example

--- 3.Basic/3.jsx-list/src/App.js
+++ 3.Basic/3.jsx-list/src/App.js
@@ -1,23 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
-
-const list = [
-  {
-    title: 'Judul 1',
-    author: 'Gun Gun Febrianza',
-    objectId: 1
-  },
-  {
-    title: 'Judul 2',
-    author: 'Rian Gustandos',
-    objectId: 2
-  },
-  {
-    title: 'Judul 3',
-    author: 'Krista Agung Ono',
-    objectId: 3
-  }
-];
+import list from './List';
 
 class App extends Component {
   render() {
---
+++ 3.Basic/3.jsx-list/src/List.js
@@ -0,0 +1,19 @@
+const list = [
+  {
+    title: 'Judul 1',
+    author: 'Gun Gun Febrianza',
+    objectId: 1
+  },
+  {
+    title: 'Judul 2',
+    author: 'Rian Gustandos',
+    objectId: 2
+  },
+  {
+    title: 'Judul 3',
+    author: 'Krista Agung Ono',
+    objectId: 3
+  }
+];
+
+export default list;
