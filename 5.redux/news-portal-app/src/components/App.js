1f20fa4fbfe2f3a2ac61b3d14b37692bd88a65a2
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-12-29 22:11:36 +0700

    Add App Component

---
+++ 5.redux/news-portal-app/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello World</p>
+      </div>
+    );
+  }
+}
+
+export default App;
