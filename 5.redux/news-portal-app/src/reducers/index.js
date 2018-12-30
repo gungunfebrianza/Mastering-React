69bc372bd7ac4857e0b17ae8985f9024263a9911
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-12-31 05:27:10 +0700

    Add Galleries

--- 5.redux/news-portal-app/src/reducers/index.js
+++ 5.redux/news-portal-app/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import articles from './article_reducer';
+import gallery from './galleries_reducer';
 
 const rootReducer = combineReducers({
-  articles
+  articles,
+  gallery
 });
 
 export default rootReducer;
